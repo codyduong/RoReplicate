d9919e6f8de2afd1777c8095e9430dab8f0191c0
Cody-Duong <duongcody507@gmail.com>
2020-06-02 18:13:36 -0500

    Update RoReplicatedBaseClass.lua

--- src/RoReplicatedBaseClass.lua
+++ src/RoReplicatedBaseClass.lua
@@ -3,6 +3,12 @@ local StandardSection = require(script.Parent.StandardSection)
 
 RoReplicateBaseClass = {}
 
+--[[
+TBD:
+whether RoReplicatedBaseClass will do plugin handling and creation itself, or will simply return itself and properties necessary
+for a properly configured plugin which has a scheme similar to the actual roblox studio?
+--]]
+
 function RoReplicateBaseClass.new(sectionCount) 
 	local self = {}
 	setmetatable(self, RoReplicateBaseClass)
@@ -14,19 +20,33 @@ function RoReplicateBaseClass.new(sectionCount)
 	
 	self._sections = StandardSection.createSections(sectionCount)
 	
+	self:_UpdateSize()
+	
 	return self
 end
 
+function RoReplicateBaseClass:SetSectionName(sectionNumber, sectionTitle)
+	self._sections:setTitle(sectionTitle)
+end
+
 function RoReplicateBaseClass:GetSizeMin()
-	return 
+	return self._sizeMin
 end
 
 function RoReplicateBaseClass:GetSizeMax()
-	return
+	return self._sizeMax
+end
+
+function RoReplicateBaseClass:GetSection(sectionNumber)
+	return self._sections[sectionNumber]
 end
 
 function RoReplicateBaseClass:_UpdateSize()
 	local sectionCount = table.length(self._sections)
 	
+	--[[todo
+	self._sizeMin = UDim2.new(0,todoVar,0,96) --height is 96-100 pixels at 1920x1080
+	self._sizeMax = UDim2.new(0,0,0,96)
+	--]]
 	
 end
