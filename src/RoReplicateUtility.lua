e0758e4f4caa51089cdad59855a2e44f78f5b304
Cody-Duong <duongcody507@gmail.com>
2020-06-20 18:36:10 -0500

    Update RoReplicateUtility.lua

--- src/RoReplicateUtility.lua
+++ src/RoReplicateUtility.lua
@@ -1,1 +1,13 @@
+local TestService = game:GetService("TestService")
 
+RoReplicateUtility = {}
+
+function RoReplicateUtility:syncBackgroundColor3(gui)
+	gui.BackgroundColor3 = Enum.StudioStyleGuideColor.MainBackground
+end
+
+function RoReplicateUtility:syncTextColor3(gui)
+	gui.TextColor3 = Enum.StudioStyleGuideColor.ButtonText
+end
+
+return RoReplicateUtility
