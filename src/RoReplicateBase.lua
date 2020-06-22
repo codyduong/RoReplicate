f0ec0e44d052715fc2f4658d8b7a7fcf143afad3
Cody-Duong <duongcody507@gmail.com>
2020-06-21 20:18:25 -0500

    Revert "0.3.0"

--- src/RoReplicateBase.lua
+++ src/RoReplicateBase.lua
@@ -1,80 +1,66 @@
 local RoReplicateUtility = require(script.Parent.RoReplicateUtility)
 local Section = require(script.Parent.Section)
-
+local TestService = game:GetService("TestService")
 
 RoReplicateBaseClass = {}
 RoReplicateBaseClass.__index = RoReplicateBaseClass
-
 
 --[[ 
 - Creates a new RoReplicateBaseClass.
 - @param pluginName - string
 - @param pluginInfo - DockWidgetPluginGuiInfo
-- @return self - RoReplicateBaseClass
 --]]
-function RoReplicateBaseClass.new(pluginName, pluginInfo)
-	assert(type(pluginName)=="string", "RoReplicateBase.new - Parameter 1 is not a string")
-	assert(getmetatable(pluginInfo)==getmetatable(DockWidgetPluginGuiInfo.new()), "RoReplicateBase.new - Parameter 2 is not a DockWidgetPluginGuiInfo")
-	
+function RoReplicateBaseClass.new(pluginName, pluginInfo) 
 	local self = {}
 	setmetatable(self, RoReplicateBaseClass)
 	
 	local frame = Instance.new("Frame")
 	RoReplicateUtility:SyncBackgroundColor3(frame)
-	frame.Size = UDim2.new(1,0,1,0)
 	--frame. I have no idea where this mutilated code came from. todo fix?
 	self._frame = frame
 	
-	local arraySect = {}
-	self._sections = arraySect
+	self._sections = {}
 	
 	self._pluginName = pluginName
-	self._tempPluginInfo = pluginInfo
 	
 	self._sizeMin = UDim2.new(0,0,0,0)
 	self._sizeMax = UDim2.new(0,0,0,0)
 	
-	local uiListLayout = Instance.new("UIListLayout", frame)
-	uiListLayout.Padding = UDim.new(1,4)
-	uiListLayout.FillDirection = Enum.FillDirection.Horizontal
-	uiListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	uiListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+	--self:_UpdateSize() --sets widget mins and maxes? This should be done manually by user through another method. As if
+	--init here it will result in nil.
+	
+	self._tempPluginInfo = pluginInfo
+	
+	--[[Due to the nature of how CreateDockWidgetPluginGui, these lines should ideally be executed last.
+	- Otherwise you cannot configure min or max size, etc, afterwards.
+	local widget = plugin:CreateDockWidgetPluginGui(pluginName, pluginInfo)
+	widget.Title = "pluginName"
+	self._widget = widget
+	self._frame.Parent = self._widget
+	]]
+	
+	--TODO: add uilayoutorder stuff to sort through sections.
 	
 	return self
 end
 
-
 --[[
-- Variadic Function
-- Adds section(s) to the RoReplicateBaseClass.
-- @param ... - Section Class
+- Adds a section to the RoReplicateBaseClass.
+- @param section - Section Class Object.
 --]]
-function RoReplicateBaseClass:AddSections(...)
-	local arg = {...}
-	for i=1, #arg do
-		assert(getmetatable(arg[i])==getmetatable(Section.new("","")), "RoReplicateBaseClass:AddSection - Parameter "..i.." is not a SectionClass")
-		if not pcall(function()
-				self._sections = table.insert(self._sections, #self._sections+1, arg[i])
-			end)
-		then
-			local arraySect = {arg[i]}
-			self._sections = arraySect
-		end
-	end
+function RoReplicateBaseClass:AddSection(section)
+	self._sections = table.insert(self._sections, #self._sections+1, section)
 end
-
 
 --[[
 - This method is executed last, it returns the plugin for the user to run.
 - Due to some complications with how plugins are handled, it is required to pass the info back, and they run it themselves.
-- Unfortunately, plugin is not a recognized enumerator outside of a plugin class
-- @return tuple - string, DockWidgetPluginGuiInfo.new()
+- Unfortunately, lugin is not a recognized enumerator outside of a plugin class
 ]]
 function RoReplicateBaseClass:ReturnPlugin()
-	self:_UpdateSize()
-	
 	local dWPGI = DockWidgetPluginGuiInfo.new(
-		self._tempPluginInfo.InitialDockState,
+	--self._tempPluginInfo.InitalDockState, it's unbelievable the InitialDockState can't be accessed but everything else can be, roblox coding oversight :(
+		Enum.InitialDockState.Float, --lets default to this until another param is accepted?
 	  	self._tempPluginInfo.InitialEnabled,
 		self._tempPluginInfo.InitialEnabledShouldOverrideRestore,
 		self._sizeMin.X.Offset,
@@ -82,33 +68,31 @@ function RoReplicateBaseClass:ReturnPlugin()
 		self._sizeMin.X.Offset,
 		self._sizeMin.Y.Offset
 	)
+	self._tempPluginInfo = nil
 	
 	return self._pluginName, dWPGI
 end
-
 
 --[[
 - This method properly initializes everything afterwards (all it does it parent all the parts of the plugin back onto itself)
 - @actualP = plugin:CreateDockWidgetPluginGui
 --]]
 function RoReplicateBaseClass:PluginRun(actualP)
-	self._frame.Parent = actualP
+	--todo
 end
-
 
 --[[
-- Internal use
+- Updates the size from # of sections.
+- Should only be used interally
 --]]
 function RoReplicateBaseClass:_UpdateSize()
-	local total = 0 --TODO
+	local sectionCount = table.length(self._sections)
 	
 	local _x = 0 --#of sections * amount of widgets in each + divider size
-	local TEMP = 1 --TODO <- remove this ugly thing
 	
-	self._sizeMin = UDim2.new(TEMP, _x, 0, 100) --height is 96-100 pixels at 1920x1080, will have to recalculate for other displays
+	self._sizeMin = UDim2.new(0, _x, 0, 96) --height is 96-100 pixels at 1920x1080, will have to recalculate for other displays
 	
-	self._sizeDef = UDim2.new(TEMP, _x, 0, 100)
+	self._sizeDef = UDim2.new(0, _x, 0, 96)
 end
-
 
 return RoReplicateBaseClass
--- src/RoReplicateUtility.lua
+++ src/RoReplicateUtility.lua
@@ -1,9 +1,7 @@
 local TestService = game:GetService("TestService")
 
-
 RoReplicateUtility = {}
 --RoReplicateUtility.__index = RoReplicateUtility
-
 
 --https://developer.roblox.com/en-us/api-reference/function/StudioTheme/GetColor
 --https://developer.roblox.com/en-us/api-reference/enum/StudioStyleGuideColor
@@ -14,10 +12,9 @@ function RoReplicateUtility:SyncBackgroundColor3(gui)
 	gui.BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.MainBackground)
 end
 
-
 function RoReplicateUtility:SyncTextColor3(gui)
 	gui.TextColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.ButtonText)
 end
 
-
 return RoReplicateUtility
+
