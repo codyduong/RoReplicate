8c2821a225b59e8d600a22c384f28fbb71d4dbf2
Cody-Duong <duongcody507@gmail.com>
2020-06-21 20:11:09 -0500

    Update _RoReplicateDemoScript.lua

--- src/_RoReplicateDemoScript.lua
+++ src/_RoReplicateDemoScript.lua
@@ -1,8 +1,9 @@
 local RoReplicate = script.Parent
 local RoReplicateBase = require(RoReplicate.RoReplicateBase)
 local RoReplicateUtility = require(RoReplicate.RoReplicateUtility)
+local RoReplicateEnum = require(RoReplicate.RoReplicateEnum)
 local Section = require(RoReplicate.Section)
-
+local Panel = require(RoReplicate.Panel)
 
 local pluginInfo = DockWidgetPluginGuiInfo.new(
 	Enum.InitialDockState.Float,  -- Widget will be initialized in floating panel
@@ -11,9 +12,16 @@ local pluginInfo = DockWidgetPluginGuiInfo.new(
 )
 
 
-local RoPlugin = RoReplicateBase.new("RoPlugin", pluginInfo)
-local Section1 = Section.new("1", "Section 1")
-RoPlugin:AddSection(Section1)
-local YourPlugin = plugin:CreateDockWidgetPluginGui(RoPlugin:ReturnPlugin())
-YourPlugin.Title = RoPlugin:ReturnPlugin()
-RoPlugin:PluginRun(YourPlugin)
+local roPlugin = RoReplicateBase.new("RoPlugin", pluginInfo)
+
+local section_1 = Section.new("1", "Section 1")
+local section_2 = Section.new("2", "Section 2")
+roPlugin:AddSections(section_1, section_2)
+
+local s1_panel_1 = Panel.new(RoReplicateEnum.Panel.Custom)
+local s1_panel_2 = Panel.new(RoReplicateEnum.Panel.ButtonImageText)
+section_1:AddPanels(s1_panel_1, s1_panel_2)
+
+local yourPlugin = plugin:CreateDockWidgetPluginGui(roPlugin:ReturnPlugin())
+yourPlugin.Title = roPlugin:ReturnPlugin()
+roPlugin:PluginRun(yourPlugin)
