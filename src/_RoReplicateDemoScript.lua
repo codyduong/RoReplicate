a62c4f4e7c4cffe49ff78d18ccb919db0dd4cbfd
Cody-Duong <duongcody507@gmail.com>
2020-06-21 19:02:30 -0500

    Update and rename RoReplicateDemoScript.lua to _RoReplicateDemoScript.lua

--- src/_RoReplicateDemoScript.lua
+++ src/_RoReplicateDemoScript.lua
@@ -3,16 +3,17 @@ local RoReplicateBase = require(RoReplicate.RoReplicateBase)
 local RoReplicateUtility = require(RoReplicate.RoReplicateUtility)
 local Section = require(RoReplicate.Section)
 
+
 local pluginInfo = DockWidgetPluginGuiInfo.new(
 	Enum.InitialDockState.Float,  -- Widget will be initialized in floating panel
 	true,   -- Widget will be initially enabled
-	false,  -- Don't override the previous enabled state
-	0,    -- Default width of the floating window
-	0,    -- Default height of the floating window
-	0,    -- Minimum width of the floating window
-	0     -- Minimum height of the floating window
+	true  -- Don't override the previous enabled state
 )
+
 
 local RoPlugin = RoReplicateBase.new("RoPlugin", pluginInfo)
 local Section1 = Section.new("1", "Section 1")
+RoPlugin:AddSection(Section1)
 local YourPlugin = plugin:CreateDockWidgetPluginGui(RoPlugin:ReturnPlugin())
+YourPlugin.Title = RoPlugin:ReturnPlugin()
+RoPlugin:PluginRun(YourPlugin)
