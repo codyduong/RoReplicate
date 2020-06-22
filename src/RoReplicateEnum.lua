96dcac40c5f1d68c36f028dd322413f138e3b046
Cody-Duong <duongcody507@gmail.com>
2020-06-21 19:03:34 -0500

    Create RoReplicateEnum.lua

---
+++ src/RoReplicateEnum.lua
@@ -0,0 +1,27 @@
+--[[
+RoReplicateEnumClass = {}
+RoReplicateEnumClass.__index = RoReplicateEnumClass
+RoReplicateEnumClass.Panel = {}
+RoReplicateEnumClass.Panel.Custom = {}
+
+
+--[[
+- TODO
+-
+--]]
+function RoReplicateEnumClass.Panel.Custom()
+	
+end
+
+
+--[[
+- TODO
+-
+--]]
+function RoReplicateEnumClass.Panel.ButtonImgageText()
+	
+end
+
+
+return RoReplicateEnumClass
+--]]
