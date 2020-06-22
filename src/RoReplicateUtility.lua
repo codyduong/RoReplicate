00f45d8d0b48a2f20d7a67ef679e130b27181c4c
Cody-Duong <duongcody507@gmail.com>
2020-06-22 03:10:09 -0500

    Update RoReplicateUtility.lua

--- src/RoReplicateUtility.lua
+++ src/RoReplicateUtility.lua
@@ -1,7 +1,9 @@
 local TestService = game:GetService("TestService")
 
+
 RoReplicateUtility = {}
 --RoReplicateUtility.__index = RoReplicateUtility
+
 
 --https://developer.roblox.com/en-us/api-reference/function/StudioTheme/GetColor
 --https://developer.roblox.com/en-us/api-reference/enum/StudioStyleGuideColor
@@ -12,9 +14,20 @@ function RoReplicateUtility:SyncBackgroundColor3(gui)
 	gui.BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.MainBackground)
 end
 
+
 function RoReplicateUtility:SyncTextColor3(gui)
 	gui.TextColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.ButtonText)
 end
 
-return RoReplicateUtility
 
+function RoReplicateUtility:SyncTitlebar(gui)
+	gui.BackgroundColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Titlebar)
+end
+
+
+function RoReplicateUtility:SyncBorderColor3(gui)
+	gui.BorderColor3 = settings().Studio.Theme:GetColor(Enum.StudioStyleGuideColor.Border)
+end
+
+
+return RoReplicateUtility
